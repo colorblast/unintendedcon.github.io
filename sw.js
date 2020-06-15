ffdab5bf53cd166264ed36c91c27737283a6fd26
Jonathan Wong <jwong123@binghamton.edu>
2020-06-15 18:54:07 +0000

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-a522df6cfa255738f249.js"
+    "url": "webpack-runtime-d9a9d9c34a18464dee7f.js"
   },
   {
     "url": "styles.6d295891da8f41c56fad.css"
@@ -46,7 +46,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "e9544e3dd3699ab51caa014428f46a6f"
+    "revision": "609dc9e09a9d9d0d59dd2bdc58dea051"
   },
   {
     "url": "manifest.webmanifest",
